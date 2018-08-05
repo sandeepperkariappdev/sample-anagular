4e6217614a3abb5d9842972c0dc19c3245052977
sandeepperkariappdev <sandeep.perkari@gmail.com>
2018-08-05 19:25:29 -0400

    updated

--- src/Components/home/home.component.ts
+++ src/Components/home/home.component.ts
@@ -10,8 +10,9 @@ import { switchMap } from 'rxjs/operators';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  constructor(public route:ActivatedRoute) { }
+  constructor(public route:ActivatedRoute, public router:Router) { }
   ngOnInit() {    
+    console.log(this.route.snapshot.paramMap.get("username")  + "Params Map get ");
     this.route.paramMap.pipe(
       switchMap((params: ParamMap) => { // (+) before `params.get()` turns the string into a number        
         +params.get('username');
@@ -21,9 +22,8 @@ export class HomeComponent implements OnInit {
     );
   }
 
-
   navToProfile(){
-    
+      //this.router.navigated([])
   }
 
 }
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes, Router } from '@angular/router';
 import {LoginComponent} from "../Components/login/login.component";
 import {HomeComponent} from "../Components/home/home.component";
-
+import {ProfileComponent} from "../Components/profile/profile.component";
 
 const routes:Routes = [
 { path:"", redirectTo:'/login', pathMatch:'full' },
 { path:"login", component:LoginComponent },
-{ path:"home", component:HomeComponent }
+{ path:"home", component:HomeComponent },
+{ path:"profile", component:ProfileComponent },
 ];
 
 
